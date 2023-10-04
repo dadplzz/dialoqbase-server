7163177b3a22d0cb41ce3914ade036d5f691109d
dadplzz <107781882+dadplzz@users.noreply.github.com>
2023-10-04 12:58:30 -0500

    Update Login.tsx

--- app/ui/src/components/Auth/Login.tsx
+++ app/ui/src/components/Auth/Login.tsx
@@ -121,14 +121,14 @@ export const AuthLogin = () => {
                     autoComplete="current-password"
                     placeholder="Password"
                     type="password"
-                    className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-3"
+                    className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6 px-3"
                   />
                 </Form.Item>
                 <div>
                   <button
                     type="submit"
                     disabled={isLoading}
-                    className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    className="flex w-full justify-center rounded-md bg-black px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-black focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
                   >
                     {isLoading ? "Loading..." : "Login"}
                   </button>
@@ -140,7 +140,7 @@ export const AuthLogin = () => {
                 Don't have an account?{" "}
                 <Link
                   to="/register"
-                  className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+                  className="font-semibold leading-6 text-black hover:text-black"
                 >
                   Register
                 </Link>
