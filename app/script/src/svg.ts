2cca0c4564f230d24d15e35c37af08d5122332a2
dadplzz <107781882+dadplzz@users.noreply.github.com>
2023-10-04 14:12:17 -0500

    Update svg.ts

--- app/script/src/svg.ts
+++ app/script/src/svg.ts
@@ -1,5 +1,5 @@
 export const OPEN_SVG =
-  `<svg xmlns="http://www.w3.org/2000/svg" style='margin: 0 auto;' width='30px' height='30px' viewBox="0 0 24 24" fill="white" stroke="currentColor" <path d="M24 1h-24v16.981h4v5.019l7-5.019h13z"/></svg>`;
+  `<svg xmlns="http://www.w3.org/2000/svg" style='margin: 0 auto;' width='30px' height='30px' viewBox="0 0 24 24" fill="white" <path d="M24 1h-24v16.981h4v5.019l7-5.019h13z"/></svg>`;
 
 export const CLOSE_SVG =
   `<svg xmlns="http://www.w3.org/2000/svg" style='margin: 0 auto;' width='30px' height='30px' fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
